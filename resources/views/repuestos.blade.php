d332e7d0148d69fd6f5d0a5dd62ac10311d9e89f
Diego Alonso Witrón López <maximusdiegoalonsodbz@gmail.com>
2025-03-18 09:54:18 -0600

    Se agregó texto de "Imagen de muestra" en la página de repuestos.blade.php

--- resources/views/repuestos.blade.php
+++ resources/views/repuestos.blade.php
@@ -3,11 +3,14 @@
 @section('title', 'Repuestos - Piaggio GT')
 
 @section('extra_head')
-    <!-- Estilos exclusivos para la página de Repuestos (si los necesitas) -->
+    <!-- Estilos exclusivos para la página de Repuestos -->
 @endsection
 
 @section('content')
-    <div class="container my-5 d-flex justify-content-center">
+    <div class="container text-center py-5" data-aos="fade-in">
+        <p class="lead text-danger">Imagen de muestra.</p>
+    </div>
+    <div class="container  d-flex justify-content-center">
         <img src="{{ asset('main/images/imagenrepuestos.png') }}" alt="Imagen de muestra" class="img-fluid" />
     </div>
 @endsection
